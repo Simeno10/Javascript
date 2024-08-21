3394822cc87a47e85bd19d14b9a95f0bcf812d89
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-08-21 19:24:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Hello World man');
+let firstName = 'Szymon';
+let lastName = 'Janiszewski';
+console.log('Hello '+firstName+' '+lastName);
+let x = 1;
+x++;
+const y = 0.3;
+console.log(x+y)
+let isApproved = false
+let secondName = undefined
+let selectedColor = null
