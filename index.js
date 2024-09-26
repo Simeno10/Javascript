0c6ab7f91d835ea0b71befcfb09a66d43cec44eb
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-09-26 21:11:17 +0200

    Update index.js
    tables and functions added

--- index.js
+++ index.js
@@ -9,4 +9,23 @@ console.log(x+y)
 let isApproved = false
 let secondName = undefined
 let selectedColor = null
+let person = {
+    name: "Szymon",
+    age: 25
+};
+person.name = "John"
+let selection = 'name'
+person[selection] = "Mary"
+console.log(person.name)
+let colors = ['red', 'yellow']
+colors[2] = 'green'
+console.log(colors.length+' '+colors[1])
+function greet(name, lastName){
+    console.log('Hello '+name+' '+lastName);
+}
+greet("Marek", 'Grechuta')
+function square(number){
+    return number*number
+}
+console.log(square(5))
 
