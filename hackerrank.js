5644826cb669ee3931c8cc919958fc5a86bfc170
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2025-05-08 11:17:25 +0200

    Update hackerrank.js

--- hackerrank.js
+++ hackerrank.js
@@ -23,34 +23,31 @@ function readLine() {
 }
 
 /**
-*   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
-*   Print three lines:
-*   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
-*   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
-*   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
+*   Calculate the area of a rectangle.
 *
-*	Parameter(s):
-*   secondInteger - The string representation of an integer.
-*   secondDecimal - The string representation of a floating-point number.
-*   secondString - A string consisting of one or more space-separated words.
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
 **/
-function performOperation(secondInteger, secondDecimal, secondString) {
-    // Declare a variable named 'firstInteger' and initialize with integer value 4.
-    const firstInteger = 4;
+function getArea(length, width) {
+    let area;
+    area = length * width
     
-    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
-    const firstDecimal = 4.0;
-    
-    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
-    const firstString = 'HackerRank ';
-    
-    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
-    console.log(Number(secondInteger)+firstInteger)
-    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
-    console.log(firstDecimal+Number(secondDecimal));
-    
-    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
-    console.log(firstString+secondString);
+    return area;
 }
 
-
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = 2 * (length + width)
+    
+    return perimeter;
+}
