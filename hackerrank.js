e0f231fa5ffc1b390e50edea82614eae9899fdd4
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2025-05-22 10:22:13 +0200

    Update hackerrank.js
    Day 2: Conditional Statements: Switch

--- hackerrank.js
+++ hackerrank.js
@@ -22,33 +22,29 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-function getGrade(score) {
-    let grade;
-    if(score<=5){
-        grade = 'F'
-    }
-    else if(score<=10){
-        grade = 'E'
-    }
-    else if(score<=15){
-        grade = 'D'
-    }
-    else if(score<=20){
-        grade = 'C'
-    }
-    else if(score<=25){
-        grade = 'B'
-    }
-    else if(score<=30){
-        grade = 'A'
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch(s[0]){
+        case 'a' || 'e' || 'i' || 'o' || 'u':
+            letter = 'A';
+            break;
+        case 'b' || 'c' || 'd' || 'f' || 'g':
+            letter = 'B'
+            break;
+        case 'h' || 'j' || 'k' || 'l' || 'm':
+            letter = 'C'
+            break;
+        default:
+            letter = 'D'
     }
     
-    return grade;
+    return letter;
 }
 
 
 function main() {
-    const score = +(readLine());
+    const s = readLine();
     
-    console.log(getGrade(score));
+    console.log(getLetter(s));
 }
