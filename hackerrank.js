08f952ded94dd4b0abc4060f51e0371c55de5b89
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2025-06-10 14:31:08 +0200

    Update hackerrank.js
    Day 4: Classes

--- hackerrank.js
+++ hackerrank.js
@@ -1,54 +1,18 @@
-'use strict';
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Return a count of the total number of objects 'o' satisfying o.x == o.y.
- * 
- * Parameter(s):
- * objects: an array of objects with integer properties 'x' and 'y'
- */
-function getCount(objects) {
-    let counter = 0;
-    for (let i = 0; i<objects.length; i++){
-        if(objects[i].x==objects[i].y){
-            counter ++;
-        }
-    }
-    
-    return counter;
+  perimeter() {
+    return this.sides.reduce((sum, side) => sum + side, 0);
+  }
 }
 
 
-function main() {
-    const n = +(readLine());
-    let objects = [];
-    
-    for (let i = 0; i < n; i++) {
-        const [a, b] = readLine().split(' ');
-        
-        objects.push({x: +(a), y: +(b)});
-    }
-    
-    console.log(getCount(objects));
-}
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
