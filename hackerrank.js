09e22a054c66e4761266cdeb0d03e28fea888101
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2025-06-20 14:11:29 +0200

    Update hackerrank.js
    Day 5: Inheritance

--- hackerrank.js
+++ hackerrank.js
@@ -1,18 +1,30 @@
-class Polygon {
-  constructor(sides) {
-    this.sides = sides;
-  }
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
 
-  perimeter() {
-    return this.sides.reduce((sum, side) => sum + side, 0);
-  }
+// Add an area method to Rectangle's prototype
+Rectangle.prototype.area = function() {
+    return this.w * this.h;
+};
+
+// Create a Square class that inherits from Rectangle
+class Square extends Rectangle {
+    constructor(side) {
+        super(side, side);
+    }
 }
 
 
-const rectangle = new Polygon([10, 20, 10, 20]);
-const square = new Polygon([10, 10, 10, 10]);
-const pentagon = new Polygon([10, 20, 30, 40, 43]);
-
-console.log(rectangle.perimeter());
-console.log(square.perimeter());
-console.log(pentagon.perimeter());
+if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
+    const rec = new Rectangle(3, 4);
+    const sqr = new Square(3);
+    
+    console.log(rec.area());
+    console.log(sqr.area());
+} else {
+    console.log(-1);
+    console.log(-1);
+}
